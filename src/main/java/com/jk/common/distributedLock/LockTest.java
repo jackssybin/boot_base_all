5171a58fa51d2b8a698d132c18760bb0c2fd2793
jackssy <27208461@qq.com>
2018-11-16 16:41:14 +0800

    1.分布式锁

--- src/main/java/com/jk/common/distributedLock/LockTest.java
+++ src/main/java/com/jk/common/distributedLock/LockTest.java
@@ -1,4 +1,18 @@
 package com.jk.common.distributedLock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LockTest {
+
+    @Autowired
+    DistributedLockHandler distributedLockHandler;
+
+    public void test(){
+        //全局分布式锁 redis setnx
+        Lock lock=new Lock("lockk","ssssssss");
+        if(distributedLockHandler.tryLock(lock)){
+            distributedLockHandler.releaseLock(lock);
+        }
+    }
+
 }
